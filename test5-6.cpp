7c5b326a8dc23c2076cd197c0ad651d471fa04c8
wanghuqing <1608751109@qq.com>
2018-03-27 16:51:34 +0800

    wrote <learning opencv>test5-6.cpp answer

---
+++ test5-6.cpp
@@ -0,0 +1,40 @@
+
+/********** <learning opencv>test5-4.cpp  *********/
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+	IplImage* src_image = cvLoadImage("imdata/binarycap.png",CV_LOAD_IMAGE_GRAYSCALE);
+	CvConnectedComp m_connect = {0};
+	CvPoint m_point = cvPoint(5,5);
+	cvNamedWindow("dstimage");
+
+	for(int y = 0;y<src_image->height;y++)
+	{
+		uchar* ptr = (uchar*)(src_image->imageData + y * src_image->widthStep);
+		for(int x = 0;x<src_image->width;x++)
+			if((int)(*ptr)==255)
+			{
+				cvFloodFill(src_image,cvPoint(x,y),cvScalar(100),cvScalar(0),cvScalar(0),&m_connect,(8|0<<8),NULL);
+
+			}
+	}
+	cout<<m_connect.area;
+	while(1)
+	{
+		cvShowImage("dstimage",src_image);
+		if(cvWaitKey(30) == 27)
+			break;
+	}
+
+	cvReleaseImage(&src_image);
+	cvDestroyWindow("dstimage");
+
+	return 0;
+}
