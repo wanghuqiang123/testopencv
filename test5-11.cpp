ca2cfaa446441a8514b4cf6b439cbcb61451202e
wanghuqing <1608751109@qq.com>
2018-04-04 11:03:29 +0800

    wrote <learning opencv>test5-11.cpp answer

---
+++ test5-11.cpp
@@ -0,0 +1,17 @@
+
+/********** <learning opencv>test5-11.cpp  *********/
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+
+using namespace std;
+using namespace cv;
+
+
+
+int main(int argc, char* argv[])
+{
+	//Œ¥’“µΩcvPyrSegmentation();
+	return 0;
+}
